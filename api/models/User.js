f5d17405f2c2cad30c1adfad0ec5908365935efa
nkapravchuk <12zvvbcy36>
2015-04-08 17:32:51 +0300

    login + session

--- api/models/User.js
+++ api/models/User.js
@@ -28,16 +28,43 @@ module.exports = {
         },
         encryptedPassword:{
             type: "string"
-        }//,
+        },
 
-//        toJSON: function(){
-//            var obj = this.toObject();
-//            delete obj.password;
-//            delete obj.encryptedPassword;
-//            delete obj.confirmation;
-//            delete obj._csrf;
-//
-//            return obj;
-//        }
+        toJSON: function(){
+            var obj = this.toObject();
+            delete obj.password;
+            delete obj.encryptedPassword;
+            delete obj.confirmation;
+            delete obj._csrf;
+
+            return obj;
+        }
+
+    },
+
+
+    findOneByEmail: function(email, callback){
+        this.findOne({"email":email}, function findByEmail(err, user){
+
+        });
+    },
+
+    beforeCreate: function(values, next){
+
+        console.log("beforeCreate")
+        if(!values.password || values.password != values.confirmation){
+            return next({err:["Password doesn't match password confirmation"]})
+        }
+
+        var bcrypt = require('bcrypt-nodejs');
+
+        bcrypt.hash(values.password, null, null, function(err, encryptedPassword) {
+            // Store hash in your password DB.
+            if(err) return next(err);
+            values.encryptedPassword = encryptedPassword;
+            values.online = true;
+            next();
+        });
+
     }
 };
